c786fb4cdf84850e314ffd210f9a67eb1713e351
Mark-Tevin <tevinsmark@gmail.com>
2024-07-25 07:18:35 -0700

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+# This script helps us setup the web server 01 and 02 for deployment
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
