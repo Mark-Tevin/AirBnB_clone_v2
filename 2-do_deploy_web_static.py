1570353eafc7b2586f686f7ff8e444af46fda663
Mark-Tevin <tevinsmark@gmail.com>
2024-07-26 06:49:27 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
+Fabric script based on the file 1-pack_web_static.py that distributes the
 archive to the web servers
 """
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['54.144.146.155', '34.229.254.170']
 
 
 def do_deploy(archive_path):
