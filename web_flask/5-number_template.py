5c7796fdfef42a86603fdce8ca8e470d79f86599
Mark-Tevin <tevinsmark@gmail.com>
2024-09-27 18:36:58 -0700

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def pythoniscool(text='is cool'):
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
+def number(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
@@ -44,4 +44,4 @@ def numbersandtemplates(n):
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
